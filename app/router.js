c1cd50602f78a81978447798c2ac28432bcd0935
Nathan Otto and Tyler Finley <Guest@epicodus-41.local>
2016-04-04 13:58:16 -0700

    add listing detail page and data

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('cat', {path: '/cat/:cat_id'});
+  this.route('listing', {path: '/listing/:listing_id'});
 });
 
 export default Router;
